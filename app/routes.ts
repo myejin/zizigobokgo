0d6cdf00dbad8d31f936cd749fd4c49edd56b860
Ellie Kim <ellie@apartmentary.com>
2025-04-06 21:47:22 +0900

    fix: csr 로 변경

--- app/routes.ts
+++
@@ -1,6 +0,0 @@
-import { type RouteConfig, route } from "@react-router/dev/routes";
-
-export default [
-    route("invitations/:invitationKey", "routes/main.tsx"),
-    // route("invitations/:invitationKey/edit", "routes/edit.tsx"),
-] satisfies RouteConfig;
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,28 @@
-import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import path from 'path';
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  plugins: [tailwindcss(), tsconfigPaths()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'components'),
     }
+  },
+  build: {
+    target: 'esnext',
+    minify: 'terser',
+    cssCodeSplit: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+        },
+      },
+    },
+  },
+  server: {
+    port: 3000,
   },
 });
