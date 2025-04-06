1a9b4761f4933902a5b6205786d44fe7899c6f3d
Ellie Kim <ellie@apartmentary.com>
2025-04-06 21:53:32 +0900

    fix: add gitignore

--- app/root.tsx
+++ app/root.tsx
@@ -1,9 +1,8 @@
-import { Routes, Route } from "react-router-dom";
-import Main from "./routes/main";
 import "./root.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Main from "./routes/main";
 
 const rootElement = document.getElementById("root");
 
