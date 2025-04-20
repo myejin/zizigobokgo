1015dc2f070e7165a0f9ca999afe644969d35849
Ellie Kim <ellie@apartmentary.com>
2025-04-20 11:25:41 +0900

    fix

--- app/root.tsx
+++ app/root.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Main from "./routes/main";
+import NotFound from "./routes/not_found";
 
 const rootElement = document.getElementById("root");
 
@@ -12,6 +13,7 @@ if (rootElement) {
     <React.StrictMode>
       <BrowserRouter>
         <Routes>
+          <Route path="" element={<NotFound />} />
           <Route path="/invitations/:invitationKey" element={<Main />} />
         </Routes>
       </BrowserRouter>
---
+++ app/routes/not_found.tsx
@@ -0,0 +1,10 @@
+const NotFound = () => {
+  return (
+    <div className="m-3">
+      <div>Hello, This is an invalid page</div>
+      <div>Happy Wedding Day!</div>
+    </div>
+  )
+}
+
+export default NotFound;
